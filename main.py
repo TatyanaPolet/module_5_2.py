776bda4f61c47b86a64927d40630dfc958f62063
TatyanaPolet <tatyana_polet@mail.ru>
2024-08-08 20:04:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
+
+h1 = House("ЖК Эльбрус", 10)
+h2 = House("ЖК .Акация", 20)
+# __str__
+print(h1)
+print(h2)
+#__len__
+print(len(h1))
+print(len(h2))
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
